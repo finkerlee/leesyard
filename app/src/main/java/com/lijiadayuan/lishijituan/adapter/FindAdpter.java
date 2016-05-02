9f1363085a7afb7503ecdba7afa9c9b6c3bf9132
nidiezaici <wangtengjob@126.com>
2016-05-02 16:29:06 +0800

    bug

--- app/src/main/java/com/lijiadayuan/lishijituan/adapter/FindAdpter.java
+++ app/src/main/java/com/lijiadayuan/lishijituan/adapter/FindAdpter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.lijiadayuan.lishijituan.adapter;
 
 import android.content.Context;
@@ -62,72 +61,4 @@ public class FindAdpter extends BaseAdapter{
         SimpleDraweeView findImageView;
     }
 }
-=======
-package com.lijiadayuan.lishijituan.adapter;
 
-import android.content.Context;
-import android.net.Uri;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-
-import com.facebook.drawee.view.SimpleDraweeView;
-import com.lijiadayuan.lishijituan.R;
-
-/**
- * Created by Administrator on 2016/4/29.
- */
-
-
-
-public class FindAdpter extends BaseAdapter {
-
-    private Context mContext;
-    private String[] mlist;
-    private int mItemId;
-
-    public FindAdpter(Context mContext, String[] mlist, int mItemId) {
-        this.mContext = mContext;
-        this.mItemId = mItemId;
-        this.mlist = mlist;
-    }
-
-    @Override
-    public int getCount() {
-        return mlist.length;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder mHolder = null;
-        if (convertView ==null){
-            mHolder = new ViewHolder();
-            convertView = View.inflate(mContext,mItemId,null);
-            mHolder.findImageView = (SimpleDraweeView) convertView.findViewById(R.id.itemImage);
-            convertView.setTag(mHolder);
-        }else{
-            mHolder = (ViewHolder) convertView.getTag();
-        }
-
-        mHolder.findImageView.setImageURI(Uri.parse(mlist[position])); //接口有数据后 用此方法获取
-
-
-//        mHolder.findImageView.setImageResource(R.drawable.fx2);
-        return convertView;
-    }
-
-    class ViewHolder{
-        SimpleDraweeView findImageView;
-    }
-}
->>>>>>> 42c4e477d66953011679224139888888180ceb1a
